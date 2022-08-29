524b915df48e764e0f482d19a5981624fef0294b
Maharani Nirmala Dewi <maharanisakti015@Gmail.com>
2022-08-29 14:59:15 +0700

    saveInstanceState

--- app/src/main/java/com/mahar/assignment1lifecycle/MainActivity.java
+++ app/src/main/java/com/mahar/assignment1lifecycle/MainActivity.java
@@ -4,22 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-//    public editTex komentar;
     public EditText komentarField;
-    String komentar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        komentar=findViewById(R.id.editTextTextMultiLine).toString();
         komentarField=findViewById(R.id.editTextTextMultiLine);
-
+        
         if (savedInstanceState != null) {
             boolean isVisible = savedInstanceState
                     .getBoolean("reply_visible");
@@ -30,47 +28,46 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
+    // onSaveInstanceState dipanggil ketika aplikasi ditutup onStop/onPause
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.i("SaveInstanceState","SaveInstanceState dipanggil");
+
         super.onSaveInstanceState(outState);
         if (komentarField.getVisibility() == View.VISIBLE) {
             outState.putBoolean("reply_visible", true);
             outState.putString("komentar", komentarField.getText().toString());
         }
     }
-
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(MainActivity.this,"siklus hidup onstart",Toast.LENGTH_SHORT).show();
+        Log.i("SaveInstanceState","siklus hidup onstart");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Toast.makeText(MainActivity.this,"siklus hidup onREstart",Toast.LENGTH_SHORT).show();
-
+        Log.i("SaveInstanceState","siklus hidup onREstart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(MainActivity.this,"siklus hidup onresume",Toast.LENGTH_SHORT).show();
+        Log.i("SaveInstanceState","siklus hidup onresume");
     }
 
 
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(MainActivity.this,"siklus hidup onPause",Toast.LENGTH_SHORT).show();
-
+        Log.i("SaveInstanceState","siklus hidup onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(MainActivity.this,"siklus hidup onstop",Toast.LENGTH_SHORT).show();
+        Log.i("SaveInstanceState","siklus hidup onstop");
 
     }
 }
